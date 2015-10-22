911205ff6afc0f548dfa301a6e133d2bb43aaa97
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2015-10-22 16:57:53 +0300

    matrix_simple

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -1,37 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int mat[4][4];
-void fillRand(int mat[4][4]);
-void rotateCW180(int mat[4][4]);
-
-int main()
-{
-int i,j;
-
-printf("Matrix fill with random numbers in range [-999..999]\n");
-fillRand(mat);
-for (i=0;i<4;i++) {
-    for (j=0;j<4;j++) {
-        printf ("%i\t",mat[i][j]);
-    }
-printf("\n");
-}
-
-printf("\n");
-
-printf("ClockWise180\n");
-rotateCW180(mat);
-for (i=0;i<4;i++) {
-    for (j=0;j<4;j++) {
-        printf ("%i\t",mat[i][j]);
-    }
-printf("\n");
-}
-
-return 0;
-}
-
 void fillRand(int mat[4][4]) {
 int i,j;
 time_t t;
@@ -44,24 +10,97 @@ for (i=0;i<4;i++) {
 }
 }
 
-
 void rotateCW180(int mat[4][4]) {
 int i,j;
+int mas[4][4];
 
-int x,y,k,g;
+for (i=0;i<4;i++) {
+    for (j=0;j<4;j++) {
+        mas[i][j]=mat[i][j];
+    }
+}
+
+int k,g,a,b;
+k=0;
+
+for (i=3;i>=0;i--) {
+    a=i-3-k;
+    g=0;
+    for (j=3;j>=0;j--) {
+    b=j-3-g;
+    g-=2;
+    mat[i][j]=mas[a][b];
+    }
+    k-=2;
+}
+}
+
+void flipV(int mat[4][4]) {
+int i,j;
+int mas[4][4];
+
+int k,g,a,b;
 
 k=0;
 for (i=3;i>=0;i--) {
-
-    x=i-3-k;
+    a=i-3-k;
     g=0;
     for (j=3;j>=0;j--) {
-
-    y=j-3-g;
+    b=j-3-g;
     g-=2;
-    mat[i][j]=mat[x][y];
+    mas[i][j]=mat[a][b];
     }
     k-=2;
 }
 
+int c,d;
+
+d=0;
+for (i=3;i>=0;i--) {
+    c=i-3-d;
+    for (j=0;j<4;j++) {
+    mat[i][j]=mas[c][j];
+    }
+    d-=2;
 }
+}
+
+void transposSide(int mat[4][4]) {
+int i,j;
+int c,d;
+int mas[4][4];
+
+for (i=0;i<4;i++) {
+    for (j=0;j<4;j++) {
+        mas[i][j]=mat[i][j];
+    }
+}
+
+d=0;
+for (i=3;i>=0;i--) {
+    c=i-3-d;
+    for (j=0;j<4;j++) {
+    mat[i][j]=mas[c][j];
+    }
+    d-=2;
+}
+
+int mas1[4][4];
+
+for (i=0;i<4;i++) {
+    for (j=0;j<4;j++) {
+        mas1[i][j]=mat[i][j];
+    }
+}
+
+int a,b;
+
+for (i=0;i<4;i++) {
+         a=3-i;
+    for (j=0;j<4;j++) {
+            b=3-j;
+        mat[i][j]=mas1[b][a];
+    }
+}
+}
+
