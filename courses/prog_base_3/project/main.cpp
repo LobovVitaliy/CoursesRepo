d625a5494caa9d925c4562ddcb91d374a8319fc6
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2016-04-03 21:55:37 +0300

    project
    Menu

--- courses/prog_base_3/project/main.cpp
+++ courses/prog_base_3/project/main.cpp
@@ -3,6 +3,9 @@
 
 using namespace sf;
 
+void settings(RenderWindow & window);
+void menu(RenderWindow & window);
+
 void menu(RenderWindow & window)
 {
     Texture Background_T, NewGame_T, LoadGame_T, SettingsGame_T, QuitGame_T;
@@ -20,20 +23,6 @@ void menu(RenderWindow & window)
     QuitGame_S.setTexture(QuitGame_T);
 
 
-
-    Texture Settings_T;
-    Settings_T.loadFromFile("Settings.png");
-
-    Sprite Settings_S;
-    Settings_S.setTexture(Settings_T);
-
-    Texture SettingsBackground_T;
-    SettingsBackground_T.loadFromFile("SettingsBackground.png");
-
-    Sprite SettingsBackground_S;
-    SettingsBackground_S.setTexture(SettingsBackground_T);
-
-
     bool isMenu = true;
     int menuNum = 0;
 
@@ -43,7 +32,7 @@ void menu(RenderWindow & window)
         Event event;
         while (window.pollEvent(event))
         {
-        // "запрос закрытия" событие: мы закрываем окно
+            // "запрос закрытия" событие: мы закрываем окно
             if (event.type == Event::Closed)
                 window.close();
         }
@@ -92,10 +81,7 @@ void menu(RenderWindow & window)
             }
             else if (menuNum == 3)
             {
-                window.draw(SettingsBackground_S);
-                window.draw(Settings_S);
-                window.display();
-                while (!Keyboard::isKeyPressed(Keyboard::Escape));
+                settings(window);
             }
             else if (menuNum == 4)
             {
@@ -122,107 +108,34 @@ void settings(RenderWindow & window)
     Sprite Settings_S;
     Settings_S.setTexture(Settings_T);
 
-    bool isMenu = true;
-    int menuNum = 0;
+    Texture SettingsBackground_T;
+    SettingsBackground_T.loadFromFile("SettingsBackground.png");
+
+    Sprite SettingsBackground_S;
+    SettingsBackground_S.setTexture(SettingsBackground_T);
 
 
-    while (isMenu)
+    while (!Keyboard::isKeyPressed(Keyboard::Escape))
     {
         // проверить все события окна, которые были вызваны с последней итерации цикла
         Event event;
         while (window.pollEvent(event))
         {
-        // "запрос закрытия" событие: мы закрываем окно
+            // "запрос закрытия" событие: мы закрываем окно
             if (event.type == Event::Closed)
                 window.close();
         }
+
+        window.draw(SettingsBackground_S);
         window.draw(Settings_S);
         window.display();
     }
-
-/*
-    while (isMenu)
-    {
-        // проверить все события окна, которые были вызваны с последней итерации цикла
-        Event event;
-        while (window.pollEvent(event))
-        {
-        // "запрос закрытия" событие: мы закрываем окно
-            if (event.type == Event::Closed)
-                window.close();
-        }
-
-        NewGame_S.setColor(Color::White);
-        LoadGame_S.setColor(Color::White);
-        Settings_S.setColor(Color::White);
-        QuitGame_S.setColor(Color::White);
-
-        menuNum = 0;
-        window.clear(Color(129, 181, 221)); // Зачем ?
-
-        if (IntRect(222, 252, 245, 78).contains(Mouse::getPosition(window)))
-        {
-            NewGame_S.setColor(Color(230, 100, 200));
-            menuNum = 1;
-        }
-        else if (IntRect(222, 340, 245, 78).contains(Mouse::getPosition(window)))
-        {
-            LoadGame_S.setColor(Color(230, 100, 200));
-            menuNum = 2;
-        }
-        else if (IntRect(222, 428, 245, 78).contains(Mouse::getPosition(window)))
-        {
-            Settings_S.setColor(Color(230, 100, 200));
-            menuNum = 3;
-        }
-        else if (IntRect(222, 516, 245, 78).contains(Mouse::getPosition(window)))
-        {
-            QuitGame_S.setColor(Color(230, 100, 200));
-            menuNum = 4;
-        }
-
-
-        if (Mouse::isButtonPressed(Mouse::Left))
-        {
-            if (menuNum == 1)
-            {
-                isMenu = false; //если нажали первую кнопку, то выходим из меню
-            }
-            else if (menuNum == 2)
-            {
-                //window.draw(about);
-                window.display();
-                while (!Keyboard::isKeyPressed(Keyboard::Escape));
-            }
-            else if (menuNum == 3)
-            {
-                //window.draw(about);
-                window.display();
-                while (!Keyboard::isKeyPressed(Keyboard::Escape));
-            }
-            else if (menuNum == 4)
-            {
-                window.close();
-                isMenu = false;
-            }
-        }
-
-        window.draw(Background_S);
-        window.draw(NewGame_S);
-        window.draw(LoadGame_S);
-        window.draw(Settings_S);
-        window.draw(QuitGame_S);
-
-        window.display();
-    }*/
 }
 
 int main()
 {
     RenderWindow window(VideoMode::getDesktopMode(), "Menu", Style::Fullscreen);
-
     menu(window);
-
     return 0;
 }
 
