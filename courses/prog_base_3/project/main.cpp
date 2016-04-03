bbece6fa5ef9a2e4c39f68f2d6226e3ea1fdd1c0
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2016-04-03 16:09:34 +0300

    project
    Menu

--- courses/prog_base_3/project/main.cpp
+++ courses/prog_base_3/project/main.cpp
@@ -1,69 +1,28 @@
 #include <SFML/Graphics.hpp>
 #include<Windows.h>
 
-void menu(sf::RenderWindow & window)
+using namespace sf;
+
+void menu(RenderWindow & window)
 {
-    sf::Texture Background;
-    Background.loadFromFile("Background.png");
+    Texture Background_T, NewGame_T, LoadGame_T, Settings_T, QuitGame_T;
+    Background_T.loadFromFile("Background.png");
+    NewGame_T.loadFromFile("NewGame.png");
+    LoadGame_T.loadFromFile("LoadGame.png");
+    Settings_T.loadFromFile("Settings.png");
+    QuitGame_T.loadFromFile("QuitGame.png");
 
-    sf::Sprite s_hero;
-    s_hero.setTexture(Background);
+    Sprite Background_S, NewGame_S, LoadGame_S, Settings_S, QuitGame_S;
+    Background_S.setTexture(Background_T);
+    NewGame_S.setTexture(NewGame_T);
+    LoadGame_S.setTexture(LoadGame_T);
+    Settings_S.setTexture(Settings_T);
+    QuitGame_S.setTexture(QuitGame_T);
 
-    bool isMenu = 1;
+
+    bool isMenu = true;
     int menuNum = 0;
 
-    while (isMenu)
-    {
-        menuNum = 0;
-        //window.clear(sf::Color(129, 181, 221));
-
-        if (sf::IntRect(100, 30, 300, 50).contains(sf::Mouse::getPosition(window)))
-        {
-            menuNum = 1;
-        }
-        //if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) {  menuNum = 2; }
-        //if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window))) {  menuNum = 3; }
-
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        {
-            if (menuNum == 1) isMenu = false;//если нажали первую кнопку, то выходим из меню
-
-        }
-
-            while (window.isOpen())
-    {
-        // проверить все события окна, которые были вызваны с последней итерации цикла
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "запрос закрытия" событие: мы закрываем окно
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::White);
-        window.draw(s_hero);
-        window.display();
-    }
-
-        window.display();
-    }
-}
-
-
-int main()
-{
-    /*int scrX = GetSystemMetrics(SM_CXSCREEN);
-    int scrY = GetSystemMetrics(SM_CYSCREEN);
-     window(sf::VideoMode (scrX, scrY), "Menu");*/
-
-    //sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Menu",  sf::Style::Fullscreen);
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Menu");
-
-    menu(window);
-
-
-// пусть программа работает до тех пор, пока открыто окно
     while (window.isOpen())
     {
         // проверить все события окна, которые были вызваны с последней итерации цикла
@@ -74,6 +33,170 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        NewGame_S.setColor(Color::White);
+        LoadGame_S.setColor(Color::White);
+        Settings_S.setColor(Color::White);
+        QuitGame_S.setColor(Color::White);
+
+        menuNum = 0;
+        window.clear(Color(129, 181, 221)); // Зачем ?
+
+        if (IntRect(222, 252, 245, 78).contains(Mouse::getPosition(window)))
+        {
+            NewGame_S.setColor(Color(230, 100, 200));
+            menuNum = 1;
+        }
+        if (IntRect(222, 340, 245, 78).contains(Mouse::getPosition(window)))
+        {
+            LoadGame_S.setColor(Color(230, 100, 200));
+            menuNum = 2;
+        }
+        if (IntRect(222, 428, 245, 78).contains(Mouse::getPosition(window)))
+        {
+            Settings_S.setColor(Color(230, 100, 200));
+            menuNum = 3;
+        }
+        if (IntRect(222, 516, 245, 78).contains(Mouse::getPosition(window)))
+        {
+            QuitGame_S.setColor(Color(230, 100, 200));
+            menuNum = 4;
+        }
+
+
+
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            if (menuNum == 1)
+            {
+                isMenu = false;//если нажали первую кнопку, то выходим из меню
+            }
+            if (menuNum == 2)
+            {
+                //window.draw(about);
+                window.display();
+                while (!Keyboard::isKeyPressed(Keyboard::Escape));
+            }
+            if (menuNum == 3)
+            {
+
+            }
+            if (menuNum == 4)
+            {
+                window.close();
+                isMenu = false;
+            }
+
+        }
+
+        window.draw(Background_S);
+        window.draw(NewGame_S);
+        window.draw(LoadGame_S);
+        window.draw(Settings_S);
+        window.draw(QuitGame_S);
+
+        window.display();
+    }
+
+
+    /*while (isMenu)
+    {
+        NewGame_S.setColor(Color::White);
+        LoadGame_S.setColor(Color::White);
+        Settings_S.setColor(Color::White);
+        QuitGame_S.setColor(Color::White);
+
+        menuNum = 0;
+        window.clear(Color(129, 181, 221)); // Зачем
+
+        if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window)))
+        {
+            NewGame_S.setColor(Color::Blue);
+            menuNum = 1;
+        }
+        if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window)))
+        {
+            LoadGame_S.setColor(Color::Blue);
+            menuNum = 2;
+        }
+        if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window)))
+        {
+            Settings_S.setColor(Color::Blue);
+            menuNum = 3;
+        }
+        if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window)))
+        {
+            QuitGame_S.setColor(Color::Blue);
+            menuNum = 4;
+        }
+
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            if (menuNum == 1)
+            {
+                isMenu = false;//если нажали первую кнопку, то выходим из меню
+            }
+            if (menuNum == 2)
+            {
+                //window.draw(about);
+                window.display();
+                while (!Keyboard::isKeyPressed(Keyboard::Escape));
+            }
+            if (menuNum == 3)
+            {
+
+            }
+            if (menuNum == 4)
+            {
+                window.close();
+                isMenu = false;
+            }
+
+        }
+
+        window.draw(Background_S);
+        window.draw(NewGame_S);
+        window.draw(LoadGame_S);
+        window.draw(Settings_S);
+        window.draw(QuitGame_S);
+
+        window.display();
+    }*/
+}
+
+
+int main()
+{
+    /*int scrX = GetSystemMetrics(SM_CXSCREEN);
+    int scrY = GetSystemMetrics(SM_CYSCREEN);
+     window(sf::VideoMode (scrX, scrY), "Menu");*/
+
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Menu",  sf::Style::Fullscreen);
+    //RenderWindow window(VideoMode::getDesktopMode(), "Menu");
+    //RenderWindow window(VideoMode(1200, 700), "Menu");
+
+    menu(window);
+
+
+    // пусть программа работает до тех пор, пока открыто окно
+    sf::RectangleShape rectangle(sf::Vector2f(120, 50));
+    // change the size to 100x100
+    rectangle.setSize(sf::Vector2f(100, 100));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(rectangle);
+        window.display();
     }
 
     return 0;
